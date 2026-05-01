da6782c2c62ab8990d40b8b974672c3ea5acc831
Lubeth <lubethjimenez@yahoo.com>
2026-05-01 16:58:08 -0400

    Add authentication using email to react app & verified app is working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,59 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import { supabase } from '../utils/supabase';
 
 export default function Header() {
+//Authentication//
+//CREATE a user in the Database//
+    const [user, setUser] = useState(null);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleSignup() {
+        console.log(username, password);
+        try {
+            const { data, error } = await supabase.auth.signUp({ email: username, password: password });
+            if(error) console.log(error);
+            console.log(data);
+            setUser(data);
+        } catch (error) {
+            console.log("Couldn't Sign Up..");    
+        }
+    }
+    async function handleLogin() {
+        try {
+            const { data, error } = await supabase.auth.signInWithPassword({ email: username,password: password });
+            if (error) console.log(error);
+            console.log(data);
+            setUser(data);
+        } catch (error) {
+            console.log("Couldn't Login..");
+        }
+    }
+    async function logout() {
+        try {
+            const { data } = await supabase.auth.signOut();
+            console.log(data);
+            setUser(null);
+        } catch (error) {
+            console.error(error);    
+        }
+    }
+    
   return (
     <header>
         <h1>Test!</h1>
+        <label htmlFor="email">Email:
+            <input type="email" name="email" id="email" value={username} 
+            onChange={(event)=> setUsername(event.target.value)}/>
+        </label>
+        <label htmlFor="password">Password:
+            <input type="password" name="password" id="password" value={password}
+            onChange={(event)=> setPassword(event.target.value)}/>
+        </label>
+        <button onClick={handleSignup}>Sign Up</button>
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={logout}>Logout</button>
     </header>
   )
 }
