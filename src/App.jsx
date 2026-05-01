4b4936e25bde5f62a5183868766c3518a1148cfe
Lubeth <lubethjimenez@yahoo.com>
2026-05-01 12:47:39 -0400

    Add READ from react app Database

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 
+import { useEffect, useState } from 'react';
+import { supabase } from './utils/supabase'
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Pets from './components/Pets';
 
 function App() {
-  
+//READ from the Database//
+  const [pets, setPets] = useState([]);
+  useEffect(() => {
+    async function getPets() {
+      try {
+        const { data } = await supabase.from("pets").select("*");
+        setPets(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getPets();
+  }, []);
 
   return (
     <>
       <h1>Workflow/Auth a project in React Database</h1>
-      <Header/>
-      <Footer/>
+      <Header />
+      <Pets pets={pets} />
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Pets.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Pets({ pets }) {
+  return (
+    <ul>
+      <h2>READ Pets Ex</h2>
+      {pets.map((pet) => (
+        <li key={pet.id}>
+          <img src={pet.image} alt={pet.description} />
+          <p>Name: {pet.name}</p>
+          <p>Type of Pet: {pet.type}</p>
+          <p>Age: {pet.age}</p>
+          <p>Breed: {pet.breed}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
